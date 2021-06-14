31cacef459ecfb53b600604ce705d13eee6622f4
Richardwillianx <richardwilliansp@gmail.com>
2021-06-14 09:23:10 -0300

    master

---
+++ operacoes.js
@@ -0,0 +1,47 @@
+function somar(){
+    var num1 = Number(document.getElementById('n1').value);
+    var num2 = Number(document.getElementById('n2').value);
+
+    document.getElementById('resultado').value = num1 + num2;
+
+}
+
+function subtrair(){
+    var num1 = Number(document.getElementById('n1').value);
+    var num2 = Number(document.getElementById('n2').value);
+
+    document.getElementById('resultado').value = num1 - num2;
+
+}
+
+function multiplicar(){
+    var num1 = Number(document.getElementById('n1').value);
+    var num2 = Number(document.getElementById('n2').value);
+
+    document.getElementById('resultado').value = num1 * num2;
+
+}
+
+
+function dividir(){
+    var num1 = Number(document.getElementById('n1').value);
+    var num2 = Number(document.getElementById('n2').value);
+
+    document.getElementById('resultado').value = num1 / num2;
+
+}
+
+function calcularDelta(){
+    var A = Number(document.getElementById('A').value);
+    var B = Number(document.getElementById('B').value);
+    var C = Number(document.getElementById('C').value);
+
+    var delta = Math.pow(B,2) - 4 * A * C;
+
+    document.getElementById('resultado').value = delta;
+}
+
+fuction calcularComprimento(){
+    var raio = Number(document.getElmentById('raio').value);
+    
+}
